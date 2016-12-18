4e64516e9e80631f3941eb3d711ef87ef522ee7a
Henry Bravo <henrybravo@Henrys-MacBook-Pro.local>
2016-12-18 11:36:58 -0500

    Practica Modulo Angular2 (Paths obligatorios)

---
+++ app/components/product/product.component.ts
@@ -0,0 +1,28 @@
+import { Component, Input, Output, EventEmitter } from "@angular/core";
+
+import { Product } from "../../models/product";
+
+@Component({
+    selector: "product",
+    templateUrl: "./app/components/product/product.component.html"
+})
+export class ProductComponent {
+
+    @Input() data: Product;
+
+    @Output() selectedProduct: EventEmitter<Product> = new EventEmitter();
+
+    notifySelectedProduct(product: Product): void {
+        this.selectedProduct.emit(product);
+    }
+    /*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|
+    | Green Path                                                       |
+    |~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|
+    | Expón un atributo de salida con el decorador correspondiente. El |
+    | tipo de dicho atributo debe permitir la emisión de eventos; la   |
+    | idea es enviar al componente padre el producto sobre el cuál se  |
+    | ha hecho clic. Y puesto que dicho clic se realiza en el template |
+    | de este componente, necesitas, además, un manejador para el      |
+    | mismo.                                                           |
+    |~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+}
